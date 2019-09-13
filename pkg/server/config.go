1f26a07cf8ab141fd9198bc9086a9a42f687566d
Gaizka DC <gaizka@imac.fohome>
2019-09-13 11:41:19 +0200

    NP-1978 repo template added

--- pkg/server/config.go
+++ pkg/server/config.go
@@ -4,5 +4,24 @@
 
 package server
 
+import (
+	"github.com/nalej/derrors"
+	"github.com/rs/zerolog/log"
+)
+
 type Config struct {
+	// incoming port
+	Port uint32
+	// Debugging flag
+	Debug bool
+
+}
+
+func (conf *Config) Validate() derrors.Error {
+
+	return nil
+}
+
+func (conf * Config) Print() {
+	log.Info().Uint32("port", conf.Port).Msg("gRPC port")
 }
