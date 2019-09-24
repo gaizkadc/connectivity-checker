2af19d66a6a4ebdfe2251a8abd200ecd96071f15
Gaizka Díaz <gdiaz@nalej.com>
2019-09-24 11:03:56 +0200

    reauthentication implemented in case of JWT expiration

--- pkg/server/connectivity-checker/checker.go
+++ pkg/server/connectivity-checker/checker.go
@@ -9,15 +9,34 @@ import (
 	"github.com/nalej/grpc-infrastructure-go"
 	"github.com/nalej/grpc-cluster-api-go"
 	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc/codes"
+	grpc_status "google.golang.org/grpc/status"
 	"time"
 )
 
 func CheckClusterConnectivity (connectivityCheckerClient grpc_cluster_api_go.ConnectivityCheckerClient, clusterAPILoginHelper login_helper.LoginHelper, clusterId *grpc_infrastructure_go.ClusterId, duration time.Duration) {
 	for true {
-		ctx, _ := clusterAPILoginHelper.GetContext()
+		ctx, cancel := clusterAPILoginHelper.GetContext()
+		if cancel != nil {
+			defer cancel()
+		}
 		_, err := connectivityCheckerClient.ClusterAlive(ctx, clusterId)
 		if err != nil {
-			log.Error().Err(err).Msg("cluster doesn't seem to be alive")
+			st := grpc_status.Convert(err).Code()
+			if st == codes.Unauthenticated {
+				errLogin := clusterAPILoginHelper.RerunAuthentication()
+				if errLogin != nil {
+					log.Error().Err(errLogin).Msg("error during reauthentication")
+				}
+				ctx2, cancel2 := clusterAPILoginHelper.GetContext()
+				defer cancel2()
+				_, err = connectivityCheckerClient.ClusterAlive(ctx2, clusterId)
+				if err != nil {
+					log.Error().Err(err).Msg("cluster doesn't seem to be alive")
+				} else {
+					log.Debug().Msg("cluster is alive")
+				}
+			}
 		} else {
 			log.Debug().Msg("cluster is alive")
 		}
